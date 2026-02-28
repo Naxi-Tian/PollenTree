46e486472ba45356fc340922eddfc4b4c9d09061
Naxi-Tian <tenne_tian@126.com>
2026-02-28 08:04:43 +0800

    Fix symptom logging crash by configuring SwiftData model container

--- PollenTree-V6/PollenTree-New.swiftpm/PollenTreeApp.swift
+++ PollenTree-V6/PollenTree-New.swiftpm/PollenTreeApp.swift
@@ -1,12 +1,13 @@
 import SwiftUI
+import SwiftData
 
 @main
 struct PollenTreeApp: App {
         var body: some Scene {
                 WindowGroup {
                         RootView()
+                            .modelContainer(for: [SymptomLog.self])
                     }
             }
 }
-
 

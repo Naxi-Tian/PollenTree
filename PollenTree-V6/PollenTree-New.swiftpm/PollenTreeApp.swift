a1cf658411a9fd4d8409f1eb5df12ff806536153
Naxi-Tian <180744936+Naxi-Tian@users.noreply.github.com>
2026-02-28 22:06:27 -0500

    Fix: Repair Package.swift manifest to resolve app target description error

--- PollenTree-V6/PollenTree-New.swiftpm/PollenTreeApp.swift
+++ PollenTree-V6/PollenTree-New.swiftpm/PollenTreeApp.swift
@@ -1,12 +1,12 @@
 import SwiftUI
+import SwiftData
 
 @main
-struct PollenTreeApp: App {
-        var body: some Scene {
-                WindowGroup {
-                        RootView()
-                    }
-            }
+struct RunPollenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+        }
+        .modelContainer(for: [SymptomLog.self])
+    }
 }
-
-
